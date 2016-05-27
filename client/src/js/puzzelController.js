83a07adf46203abb9305b81a99311171fce789bc
Niek Schmoller <n.schmoller@nerds.company>
2016-05-27 17:11:27 +0200

    hint numbering

--- client/src/js/puzzelController.js
+++ client/src/js/puzzelController.js
@@ -8,13 +8,13 @@ module.exports = [
     $scope.tips = [
       "Tip 1: Druk op de groene knop voor meer tips",
       "Tip 2: Schrijf de kaarten op papier, met tekens",
-      "Tip 2: Alle kaarten vormen samen een som",
-      "Tip 3: Niet alle kaarten zijn programmeertalen",
-      "Tip 4: Vervang de tekens door de cijfers",
-      "Tip 5: De * bij javascript is vermenigvuldigen",
-      "Tip 6: Er zijn in totaal 14 kaarten",
-      "Tip 7: Van de 14 kaarten heb je er 11 nodig",
-      "Tip 8: Bali, violet en wurgslang zijn GEEN programmeertalen",
+      "Tip 3: Alle kaarten vormen samen een som",
+      "Tip 4: Niet alle kaarten zijn programmeertalen",
+      "Tip 5: Vervang de tekens door de cijfers",
+      "Tip 6: De * bij javascript is vermenigvuldigen",
+      "Tip 7: Er zijn in totaal 14 kaarten",
+      "Tip 8: Van de 14 kaarten heb je er 11 nodig",
+      "Tip 9: Bali, violet en wurgslang zijn GEEN programmeertalen",
       "Nu moet je het echt zelf doen. De tips zijn op!"
     ];
 
@@ -33,7 +33,7 @@ module.exports = [
         $rootScope.playBuzzer();
         $scope.notification = $scope.tips[$scope.tip];
         $scope.tip = $scope.tip + 1;
-        if ($scope.tip === 9) {
+        if ($scope.tip === 10) {
           $scope.tip = 0;
         }
       }
